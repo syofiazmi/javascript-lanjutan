b0b7f40d881575e723893ba6b3c9df2b5424ee57
M. Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-02-23 13:16:49 +0800

    add Arrow Function Completed

--- arrow function/script.js
+++ arrow function/script.js
@@ -50,5 +50,110 @@ let mahasiswa = ['Syofi', 'Subhan', 'Hafiz'];
 
 // kalau mau mengembalikannya dalam bentuk object
 
-let jumlahHuruf = mahasiswa.map(nama => ({nama, jmlHuruf: nama.length}));
-console.table(jumlahHuruf)
+// let jumlahHuruf = mahasiswa.map(nama => ({nama, jmlHuruf: nama.length}));
+// console.table(jumlahHuruf)
+
+
+
+
+// Konsep this pada Arrow Function
+
+// kita coba bikin object
+
+// pakai Constructor Function
+
+// const Mahasiswa = function(){
+//     this.nama = 'Syofi';
+//     this.umur = 33;
+//     this.sayHello = function(){
+//         console.log(`Halo nama saya ${this.nama}, saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const syofi = new Mahasiswa();
+
+
+
+// ganti jadi Arrow Function
+
+// const Mahasiswa = function(){
+//     this.nama = 'Syofi';
+//     this.umur = 33;
+//     this.sayHello = () => {
+//         console.log(`Halo nama saya ${this.nama}, saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const syofi = new Mahasiswa();
+
+
+
+
+// pakai Object Literal
+
+// const mhs1 = {
+//     nama: 'Syofi',
+//     umur: 26,
+//     sayHello: () => {
+//         // console.log(`Halo nama saya ${this.nama}, saya ${this.umur} tahun.`);
+//         console.log(this);
+//     }
+// } 
+
+
+
+
+
+
+
+
+
+// Constructor Function
+
+// const Mahasiswa = function(){
+//     this.nama = 'Syofi';
+//     this.umur = 33;
+//     this.sayHello = function(){
+//         console.log(`Halo nama saya ${this.nama}, saya ${this.umur} tahun.`);
+//     }
+
+//     setInterval( () => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+
+// const syofi = new Mahasiswa();
+
+
+
+
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if (this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+    }
+    
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    },600);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
