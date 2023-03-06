08f6e1b67bd3c20a19421e169c1e16c921d1a679
M. Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-03-06 18:37:32 +0800

    add materi async-await

---
+++ async - await/script.js
@@ -0,0 +1,34 @@
+function cobaPromise() {
+    return new Promise((resolve, reject) => {
+        const waktu = 5000;
+        if (waktu < 5000) {
+            setTimeout(() => {
+                resolve('selesai')
+            }, waktu);
+        } else {
+            reject('kelamaan!');
+        }
+    });
+}
+
+// const coba = cobaPromise();
+// coba
+//     .then(coba => console.log(coba))
+//     .catch(coba => console.log(coba));
+
+
+
+async function cobaAsync() {
+    try {
+        const coba = await cobaPromise();
+        console.log(coba);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+cobaAsync();
+
+
+
+
